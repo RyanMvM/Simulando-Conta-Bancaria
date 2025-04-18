e7e70ea6ad9a603f6932cb241ab332b9e99fddaa
RyanPacheco <oryanpacheco@gmail.com>
2025-04-17 22:46:06 -0300

    Estrutura inicial do pacote Conta com funcionalidades de operação bancária

---
+++ src/conta/ContaBancaria.java
@@ -0,0 +1,56 @@
+package conta;
+
+public class ContaBancaria {
+
+	private String titular;
+	private String numeroConta;
+	private double saldo;
+
+	public ContaBancaria(String titular, String numeroConta, int saldo) {
+
+		this.titular = titular;
+		this.numeroConta = numeroConta;
+		this.saldo = saldo;
+
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public String getNumeroConta() {
+		return numeroConta;
+	}
+
+	public void setNumeroConta(String numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+
+	public boolean sacar(double valor) {
+
+	boolean sucesso = (saldo >= valor); 
+	saldo = sucesso ? saldo - valor : saldo; 
+	return sucesso; 
+	
+	}
+
+	public String toString() {
+
+		return "/nNome: " + this.getTitular() + "/nNumero da COnta: " + this.getNumeroConta() + "/nSaldo: "
+				+ this.getSaldo();
+
+	}
+
+}
